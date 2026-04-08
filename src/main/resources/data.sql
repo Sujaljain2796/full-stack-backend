33d2aba70721a51bdd842171d3f27197a05cb969
Sujal Jain <23bai70440@cuchd.in>
2026-04-08 10:20:14 +0530

    Initial commit - Spring Boot RBAC experiment

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (id, username, password, role) VALUES
+                                                     (1, 'user1', '{noop}123', 'ROLE_USER'),
+                                                     (2, 'admin1', '{noop}123', 'ROLE_ADMIN');
